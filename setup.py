a992df684e92de41679f451db4ad778154442673
Pierre-louis <tissotfavrepierrelouis@gmail.com>
2025-01-14 14:28:03 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Text-based-Adventure-Game',
+    version='1.0',
+    author='pierrelouistissot',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
